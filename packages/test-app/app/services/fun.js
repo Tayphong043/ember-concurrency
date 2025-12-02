73df2db6d9ba38e93bde6af5c33f0781d7f4bdf7
Tayphong043 <traingoan043@gmail.com>
2025-12-02 22:37:26 +0700

    Delete packages/test-app directory

--- packages/test-app/app/services/fun.js
+++
@@ -1,5 +0,0 @@
-import Service from '@ember/service';
-
-export default class FunService extends Service {
-  foo = 123;
-}
--- packages/test-app/snippets/ts/basic-example.ts
+++
@@ -1,9 +0,0 @@
-import Component from '@glimmer/component';
-import { task, timeout } from 'ember-concurrency';
-
-export default class extends Component {
-  myTask = task(async (ms: number) => {
-    await timeout(ms);
-    return 'done!';
-  });
-}
